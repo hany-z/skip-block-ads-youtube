d681408c9654dfaa21b268074bad22c935cf38c8
Thiên Ân ID <thienanid@MacBook-Pro-cua-Thien.local>
2024-06-30 00:17:07 +0700

    update

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ var donate = document.getElementById("donate");
 var back = document.getElementById("back");
 var qr = document.getElementById("qr");
 
+const momo = document.getElementById("momo");
+const usdt = document.getElementById("usdt");
+
+const momo_btn = document.getElementById("momo_btn");
+const usdt_btn = document.getElementById("usdt_btn");
+
 donate.addEventListener("click", function () {
      qr.style.display = "block";
 });
 
 back.addEventListener("click", function () {
      qr.style.display = "none";
+});
+
+momo_btn.addEventListener("click", function () {
+     momo.style.display = "block";
+     usdt.style.display = "none";
+});
+
+usdt_btn.addEventListener("click", function () {
+     usdt.style.display = "block";
+     momo.style.display = "none";
 });
