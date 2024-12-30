4f1f3cee3fc460cb82ca6f9679f8e7171c244daa
Hany <quenmattroi@gmail.com>
2024-12-31 00:36:12 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,31 +1,46 @@
 function skipAd() {
-     //skip 1
-     const skipAdsManual =
-          document.getElementsByClassName("ytp-skip-ad-button");
-     if (skipAdsManual && skipAdsManual.length > 0) {
-          skipAdsManual[0].click();
-          return;
-     }
-
      //skip 2
      const adsShow = document.getElementsByClassName(
-          "ytp-ad-player-overlay-layout",
-     );
-     const skipAdsAuto = document.getElementsByClassName(
           "video-stream html5-main-video",
+     );
+     const adsDisplay = document.getElementsByClassName(
+          "ytp-ad-player-overlay-layout",
      );
      if (
           adsShow &&
+          adsDisplay &&
           adsShow.length > 0 &&
-          skipAdsAuto &&
-          skipAdsAuto.length > 0
+          adsDisplay.length > 0
      ) {
-          skipAdsAuto[0].currentTime = skipAdsAuto[0].duration;
+          console.log("skip 2");
+          adsShow[0].currentTime = adsShow[0].duration;
+          return;
+     }
+
+     //skip 3
+     const adsShow2 = document.getElementsByClassName(
+          "video-stream html5-main-video",
+     );
+     const adsDisplay2 = document.getElementsByClassName(
+          "video-ads ytp-ad-module",
+     );
+     if (
+          adsShow2 &&
+          adsDisplay2 &&
+          adsShow2.length > 0 &&
+          adsDisplay2.length > 0 &&
+          adsDisplay2[0].innerHTML.length > 0
+     ) {
+          console.log("skip 3");
+          adsShow2[0].currentTime = adsShow2[0].duration;
           return;
      }
 }
 
-setInterval(() => {
-     // console.log("Checking for ads...");
+console.log("Checking for ads...");
+skipAd();
+
+const loop = setInterval(() => {
+     console.log("Checking for ads...");
      skipAd();
 }, 500);
